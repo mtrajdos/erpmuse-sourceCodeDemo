67d9183988637858941729af19e1be87f25dc470
Michal Trajdos <michal.trajdos95@gmail.com>
2024-09-26 13:20:31 +0200

    Added fixed settings

--- KivyTestApp.py
+++ KivyTestApp.py
@@ -1,3 +1,12 @@
+from kivy.config import Config
+
+# Example settings
+Config.set('graphics', 'multisampling', '0')  # Disable multisampling for testing
+Config.set('graphics', 'width', '800')  # Set window width
+Config.set('graphics', 'height', '600')  # Set window height
+Config.set('graphics', 'fullscreen', '0')  # Disable fullscreen for testing
+
+# After setting configuration, run the app
 from kivy.app import App
 from kivy.uix.label import Label
 
